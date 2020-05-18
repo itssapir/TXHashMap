4cd4a2f4fb494b0e862ae5d0e2f971617c089fe2
Sapir <itssapir@gmail.com>
2020-05-18 19:19:10 +0300

    added basic skeleton for HashMap dummy implementation

---
+++ DummyImplementation/src/dummyImplementation/TXData.java
@@ -0,0 +1,10 @@
+package dummyImplementation;
+
+public class TXData {
+    // TODO: read-set
+    // TODO: write-set
+    boolean inTX;
+    int txTime;
+    // TODO: what happens in resize()?
+    //          soft/aggressive resize approach?
+}
--- DummyImplementation/src/dummyImplementation/TXHashMap.java
+++ DummyImplementation/src/dummyImplementation/TXHashMap.java
@@ -1,8 +1,75 @@
 package dummyImplementation;
 
+//import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TXHashMap<K,V> {
+
+    // Node structure
+    static class Node<K,V> {
+        final int hash;
+        final K key;
+        V value;
+        int updateTime;
+        Node<K,V> next;
+
+        Node(int hash, K key, V value, int updateTime, Node<K,V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public final K getKey() {
+            return key;
+        }
+        public final V getValue() {
+            return value;
+        }
+        public final String toString() {
+            return key + "=" + value;
+        }
+        
+        public final V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+    }
+
+    static class NodeList<K,V> {
+        Node<K,V> head;
+        ReentrantLock lock;
+        int updateTime;
+
+        NodeList() {
+            this.head = null;
+            this.updateTime = 0;
+            lock = new ReentrantLock();
+        }
+        public final int getUpdateTime() {
+            return updateTime;
+        }
+    }
+
+    static private AtomicInteger GVC = new AtomicInteger();
+    private NodeList<K,V>[] table;
+    private int size;
+    static private ThreadLocal<Integer /*TODO: data structure for each thread*/> TXData = new ThreadLocal<>();    
+
+    
     public TXHashMap() {
 
+    }
+
+    public void TXBegin() {
+        
+    }
+
+
+    public void TXCommit() {
+        
     }
 
     public void clear() {
