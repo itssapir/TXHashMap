90fb282052ce6c78189f5a086da3e3eca876b920
Sapir <itssapir@gmail.com>
2020-05-18 13:15:38 +0300

    first commit

---
+++ src/dummyImplementation/TXHashMap.java
@@ -0,0 +1,44 @@
+package dummyImplementation;
+
+public class TXHashMap<K,V> {
+    public TXHashMap() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+
+    public V get(Object key) {
+        return null;
+    }
+
+    public V put(K key, V value) {
+        return null;
+    }
+
+    public V remove(Object key) {
+        return null;
+    }
+
+
+
+    // TODO: other methods as required.
+}
