c01ce5d453b21589dd66ce360edc40496da95015
Sapir <itssapir@gmail.com>
2020-08-08 18:23:09 +0300

    added initialSize and table fields to be used for resize functionality

--- transactionLibClosedNesting/src/main/java/LocalHashMap.java
+++ transactionLibClosedNesting/src/main/java/LocalHashMap.java
@@ -3,6 +3,8 @@ import java.util.HashSet;
 
 public class LocalHashMap {
 	public int sizeDiff = 0;
+	public int initialSize;
+	public HashNodeList[] table;
 	public HashSet<HashNodeList> hashReadSet = new HashSet<>();
 	public HashMap<HashNodeList, HashMap<Object, HashNode>> hashWriteSet = new HashMap<>();
 }
